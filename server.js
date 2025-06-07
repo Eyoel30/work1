a827cff5f85d83dc637f4b0dcaad24fedbe523cf
eyeol <eyoelzewde23@gmail.com>
2025-06-07 19:30:35 +0300

    linek data base

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import pg from 'pg'
+
+const app = express()
+const port = 3000
+const db = new pg.Client({
+  user: "postgrsql",
+  host: "localhost",
+  database: "permalist",
+  password: "789456",
+  port: 5432,
+})
+
+db.connect()
+
+app.listen(port,()=>{
+  console.log(`server is running on ${port}`)
+})
