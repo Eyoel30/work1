3731f3979826c3e287ece9e23d551e8609679fb6
eyeol <eyoelzewde23@gmail.com>
2025-06-09 14:19:55 +0300

    setting up the root api access

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express'
-
+import { router } from './routes/customerRoutes'
 const app = express()
 const port = 3000
-
+app.use(express.json())
+app.use('/api/access', router)
 
 app.listen(port,()=>{
   console.log(`server is running on ${port}`)
